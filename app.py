b5b5082781f85464c61cd90e48f420538d7f8493
harryjulian <harryjulian@users.noreply.huggingface.co>
2025-10-02 16:33:39 +0200

    first attempt

--- app.py
+++ app.py
@@ -1,7 +1,40 @@
+import sys
+sys.path.append("neutts-air")
+from neuttsair.neutts import NeuTTSAir
 import gradio as gr
+import spaces
 
-def greet(name):
-    return "Hello " + name + "!!"
+# load model
+tts = NeuTTSAir(
+    backbone_repo=backbone,
+    backbone_device="gpu",
+    codec_repo="neuphonic/neucodec",
+    codec_device="gpu"
+)
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-demo.launch()
+@spaces.GPU()
+def infer(ref_text, ref_audio_path, gen_text):
+
+    gr.Info("Starting inference request!")
+    gr.Info("Encoding reference...")
+    ref_codes = tts.encode_reference(ref_audio_path)
+
+    gr.Info(f"Generating audio for input text: {input_text}")
+    wav = tts.infer(input_text, ref_codes, ref_text)
+
+    return (24_000, wav)
+
+demo = gr.Interface(
+    fn=infer,
+    inputs=[
+        gr.Textbox(label="Reference Text"),
+        gr.Audio(source="upload", type="filepath", label="Reference Audio"),
+        gr.Textbox(label="Text to Generate"),
+    ],
+    outputs=gr.Audio(type="numpy", label="Generated Speech"),
+    title="NeuTTS-Air",
+    description="Upload a reference audio sample, provide the reference text, and enter new text to synthesize."
+)
+
+if __name__ == "__main__":
+    demo.launch()
